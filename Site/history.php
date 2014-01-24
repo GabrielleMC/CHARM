b4b6d850818a9fd1ef882594cc7c3c4cd7b32743
Gabbeh <gabichan@me.com>
2014-01-24 15:06:21 -0700

    Added charts and datetime axis
    Axis needs to be fixed to properly get date.
    FEAR the hworf of data.

--- Site/history.php
+++ Site/history.php
@@ -14,16 +14,37 @@
     mysql_select_db("testCHARM");
 
     $result = mysql_query("SELECT logtime, value FROM t1");
+    $result2 = mysql_query("SELECT logtime, value FROM t2");
+    $result3 = mysql_query("SELECT logtime, value FROM t3");
 
     while ($row = mysql_fetch_array($result)) {
         extract($row);
         $data[] = "[$value]";
+        $time[] = "$logtime";
     }
+    
+    while ($row2 = mysql_fetch_array($result2)) {
+        extract($row2);
+        $data2[] = "[$value]";
+        //$time[] = "$logtime";
+    }
+    
+    while ($row3 = mysql_fetch_array($result3)) {
+        extract($row3);
+        $data3[] = "[$value]";
+        //$time[] = "$logtime";
+    }
+    
     mysql_free_result($result);
+    mysql_free_result($result2);
+    mysql_free_result($result3);
+    
 ?>
 
 <script type="text/javascript">
     $(function () {
+        var t = "<?php echo $time[0] ?>".split(/[- :]/);
+        var d = Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
         $('#container').highcharts({
             title: {
                 text: 'Sample Home Data',
@@ -33,7 +54,10 @@
                 }
             },
             xAxis: {
-                tickInterval: 250
+                type: 'datetime',
+                dateTimeLabelFormats: {
+                    day: '%e of %b'
+                }
             },
             yAxis: { // left y axis
                 title: {
@@ -64,7 +88,18 @@
             	
             series: [{
                 data :[<?php echo join($data, ',') ?>],
+                pointInterval: 60*10, // ten minutes
                 color: '#2191C0'
+            },
+            {
+                data :[<?php echo join($data2, ',') ?>],
+                pointInterval: 60*10, // ten minutes
+                color: '#FF3030'
+            },
+            {
+                data :[<?php echo join($data3, ',') ?>],
+                pointInterval: 60*10, // ten minutes
+                color: '#33cc33'
             }]
         });
     });
