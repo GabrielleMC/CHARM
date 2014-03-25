e74bf8ad8747ace2da471749bb17192cdc649c2d
Gabbeh <gabichan@me.com>
2014-03-24 20:15:45 -0600

    Small fix to devices

--- Virtual_Implementation/common/device.cpp
+++ Virtual_Implementation/common/device.cpp
@@ -640,11 +640,11 @@ int Device::update_db_status(){
 		//Add the new tables
 		ss.str(std::string());
     	ss << "CREATE TABLE `Device_" << id << "` (`value` double DEFAULT NULL,`logtime` datetime NOT NULL, PRIMARY KEY (logtime))";
-		const char *query = str.c_str();
+		query = str.c_str();
 		mysql_query(connect, query);
 		ss.str(std::string());
 		ss << "CREATE TABLE `Device_Day_" << id << "` (`logdate` date NOT NULL, `total` double DEFAULT NULL, PRIMARY KEY (logdate))";
-		const char *query = str.c_str();
+		query = str.c_str();
 		mysql_query(connect, query);
 		
 	}
