f6a39054b802703e541d9dcd4e3655c4e9d51f48
lukey-luke <balgarath@gmail.com>
2014-03-31 00:37:39 -0600

    resolved wlan.h conflict

--- Embedded_Voltmeter/Basic WiFi Source/Basic WiFi Application CCS/BasicWiFi Application/Debug/Library/subdir_vars.mk
+++ Embedded_Voltmeter/Basic WiFi Source/Basic WiFi Application CCS/BasicWiFi Application/Debug/Library/subdir_vars.mk
@@ -5,7 +5,6 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 LIB_SRCS += \
 E:/Dropbox/git/CHARM/Embedded_Voltmeter/Basic\ WiFi\ Source/Basic\ WiFi\ Application\ CCS/CC3000\ Spi/Debug/CC3000\ Spi.lib \
-E:/Dropbox/git/CHARM/Embedded_Voltmeter/Basic\ WiFi\ Source/Basic\ WiFi\ Application\ CCS/CC3000HostDriver/Debug/CC3000HostDriver.lib \
-E:/Dropbox/git/CHARM/Embedded_Voltmeter/Basic\ WiFi\ Source/Basic\ WiFi\ Application\ CCS/HyperTerminal\ Driver/Debug/HyperTerminal\ Driver.lib 
+E:/Dropbox/git/CHARM/Embedded_Voltmeter/Basic\ WiFi\ Source/Basic\ WiFi\ Application\ CCS/CC3000HostDriver/Debug/CC3000HostDriver.lib 
 
 
